ff487da81903f63f129153ea2461ffe9634a9452
KacemJR <98021012+KacemJR@users.noreply.github.com>
2025-02-13 17:04:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import Home from "./pages/Home";
-import "./App.css"; // Si tu veux ajouter des styles personnalisés
+import React from 'react';
+import Home from './pages/Home';
+import './App.css';
+import { BookProvider } from './context/BookContext';
 
 function App() {
-  return <Home />;
+    return (
+        <BookProvider>
+            <Home />
+        </BookProvider>
+    );
 }
 
 export default App;
