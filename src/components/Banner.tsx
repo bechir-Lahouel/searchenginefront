b826b4d6d8cc4d7ae6d3ef390f3bed91e1b429a4
CalvinMP <91535810+CalvinMP@users.noreply.github.com>
2025-02-14 11:20:20 +0100

    La barre de recherche par titre fonctionne

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,14 @@
+import { useState, useContext } from "react";
+import { BookContext } from '../context/BookContext';
+
 const Banner = () => {
+    const [query, setQuery] = useState("");
+    const { setFilters } = useContext(BookContext)!;
+    
+    const handleSearch = () => {
+      setFilters(prevFilters => ({ ...prevFilters, title: query }));
+    };
+
     return (
       <div className="banner">
         {/* Texte et barre de recherche */}
@@ -6,10 +16,25 @@ const Banner = () => {
           <h1>
             Once you learn to read, you will be forever free
           </h1>
-          <div className="search-bar">
-            <input type="text" placeholder="Search a book, an author or genre" />
-            <button>Search</button>
-          </div>
+            <div className="search-bar">
+            <input 
+              type="text" 
+              placeholder="Search a book, an author or genre" 
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSearch();
+                }
+              }}
+              onKeyUp={(e) => {
+                if (e.currentTarget.value === "") {
+                  setFilters(prevFilters => ({ ...prevFilters, title: "" }));
+                }
+              }}
+              />
+            <button onClick={handleSearch}>Search</button>
+            </div>
         </div>
   
         {/* Image illustrant la lecture */}
